520a42d75751e056b6473961a5f561591d022cea
AndresGalloM <felipegallom1011@gmail.com>
2023-03-16 23:03:09 -0500

    Clase 4 evento submit con form

--- js_basico_practico/script.js
+++ js_basico_practico/script.js
@@ -1,13 +1,19 @@
 const calculo1 = document.querySelector('#calculo1');
 const calculo2 = document.querySelector('#calculo2');
 const resultado = document.querySelector('#resultado');
+const btn = document.querySelector('#btn-calcular')
+const formulario = document.querySelector('#formulario')
 
-function calcular() {
+// btn.addEventListener('click', calcular)
+formulario.addEventListener('submit', calcular)
+
+function calcular(event) {
+    event.preventDefault()
     let num1 = Number(calculo1.value);
-    let num2 = !Number(calculo2.value);
+    let num2 = Number(calculo2.value);
 
-    if (!num1 || num2) {
-        resultado.innerText = 'Error: Alguno de los inputs biene vacio';
+    if (!num1 || !num2) {
+        resultado.innerText = 'Error: Alguno de los inputs biene vacio, o esta mal';
         return;
     }
 
